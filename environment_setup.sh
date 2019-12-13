ecc25a2b30461d3ea7cf0efe77adb711c9c8441b
root <root@bastion.cbc0.example.opentlc.com>
2019-12-13 01:43:39 +0000

    All - Good

--- environment_setup.sh
+++ environment_setup.sh
@@ -14,4 +14,4 @@ JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
 REGION=us-east-1
 RH_MAIL_ID=hgunasin@au1.ibm.com
 
-ansible-playbook main_playbook.yml --private-key=/root/.ssh/mykey.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+ansible-playbook main_playbook.yml --private-key=/root/.ssh/mykey.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO} -vv
